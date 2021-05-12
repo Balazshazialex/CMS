1bc8095443d4fce61ef67dbf91978a5846c68c0d
adabi <adabirtocian@yahoo.com>
2021-05-12 11:24:02 +0300

    add: linked UploadProposal.fxml to AfterLoginAuthor.fxml

--- src/sample/UploadProposal.java
+++ src/sample/UploadProposal.java
@@ -1,21 +1,25 @@
 package sample;
 
 import Controllers.ProposalController;
+import Model.Conference;
+import Model.ConferenceParticipant;
 import Model.Proposal;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class UploadProposal implements Initializable {
 
     private ProposalController proposalController;
-    int authorId = 1;
-    int conferenceId = 1;
+    private Conference conference;
+    private ConferenceParticipant  author;
 
     @FXML
     private TextField name;
@@ -44,12 +48,11 @@ public class UploadProposal implements Initializable {
     @FXML
     private Button updateButton;
 
+    @FXML
+    private Button backButton;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        this.proposalController = new ProposalController();
-        Proposal proposal = proposalController.findOneByAuthorIdConferenceId(authorId, conferenceId);
-        if(proposal != null) this.populateTextFields(proposal);
-        else this.updateButton.setVisible(false);
     }
 
     private void populateTextFields(Proposal proposal) {
@@ -65,21 +68,42 @@ public class UploadProposal implements Initializable {
     }
 
     public void addProposal() {
-        String name = this.name.getText();
-        String abstractPaper = this.abstractPaper.getText();
-        String fullPaper = this.fullPaper.getText();
-        String keywords = this.keywords.getText();
-        String topics = this.topics.getText();
-        String listOfAuthors = this.listOfAuthors.getText();
-        String metaInfo = this.metaInfo.getText();
+        Date dateNow = new Date();
 
-        Proposal proposal = new Proposal(conferenceId, authorId, name, listOfAuthors, metaInfo, abstractPaper,
-                fullPaper, keywords, topics);
-        this.proposalController.add(proposal);
-        this.populateTextFields(proposal);
+        if(dateNow.before(this.conference.getCallForPapers())) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Call for papers not started yet", ButtonType.OK);
+            alert.showAndWait();
+        }
+        else if(dateNow.after(this.conference.getProposalDeadline())) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Past deadline for uploading", ButtonType.OK);
+            alert.showAndWait();
+
+        } else {
+            String name = this.name.getText();
+            String abstractPaper = this.abstractPaper.getText();
+            String fullPaper = this.fullPaper.getText();
+            String keywords = this.keywords.getText();
+            String topics = this.topics.getText();
+            String listOfAuthors = this.listOfAuthors.getText();
+            String metaInfo = this.metaInfo.getText();
+
+            Proposal proposal = new Proposal(conference.getId(), this.author.getId(), name, listOfAuthors, metaInfo, abstractPaper,
+                    fullPaper, keywords, topics);
+            this.proposalController.add(proposal);
+            this.populateTextFields(proposal);
+        }
     }
 
     public void updateProposal() {
+        Date dateNow = new Date();
+        if(dateNow.after(this.conference.getProposalDeadline())) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Past deadline for uploading", ButtonType.OK);
+            alert.showAndWait();
+
+        } else if(! this.fullPaper.getText().equals("") && dateNow.after(this.conference.getFullpaperDeadline())) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Past deadline for full paper", ButtonType.OK);
+            alert.showAndWait();
+        }
         String name = this.name.getText();
         String abstractPaper = this.abstractPaper.getText();
         String fullPaper = this.fullPaper.getText();
@@ -88,9 +112,34 @@ public class UploadProposal implements Initializable {
         String listOfAuthors = this.listOfAuthors.getText();
         String metaInfo = this.metaInfo.getText();
 
-        Proposal proposal = new Proposal(conferenceId, authorId, name, listOfAuthors, metaInfo, abstractPaper,
+        Proposal proposal = new Proposal(conference.getId(), this.author.getId(), name, listOfAuthors, metaInfo, abstractPaper,
                 fullPaper, keywords, topics);
         this.proposalController.update(proposal);
         this.populateTextFields(proposal);
+    }
+
+    public void setConference(Conference conference, ConferenceParticipant author) {
+        this.conference = conference;
+        this.author = author;
+        System.out.println(conference);
+        this.proposalController = new ProposalController();
+        Proposal proposal = proposalController.findOneByAuthorIdConferenceId(this.author.getId(), this.conference.getId());
+
+
+        if(proposal != null) this.populateTextFields(proposal);
+        else this.updateButton.setVisible(false);
+    }
+
+    public void goBack() {
+        String nextScreen = "/sample/AfterLoginAuthor.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(nextScreen));
+        try {
+            Parent parent = loader.load();
+            this.backButton.getScene().setRoot(parent);
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, e.getMessage(), ButtonType.OK);
+            alert.showAndWait();
+        }
     }
 }
